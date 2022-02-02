ee94bfb487f7375f19d4edbd0ea6cf06bfeb4e64
Michael <ndiritumichael5@gmail.com>
2022-02-02 15:07:21 +0300

    added a function to reset system bar colors

---
+++ common-compose/src/main/java/dev/mike/commons/utils/ResetSytemBarsColors.kt
@@ -0,0 +1,19 @@
+package dev.mike.commons.utils
+
+import android.util.Log
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+@Composable
+fun ResetSystemBars() {
+    val uiController = rememberSystemUiController()
+    val darkIcons = isSystemInDarkTheme().not()
+    LaunchedEffect(key1 = true, block = {
+
+        Log.d("resetbars","colors reset")
+        uiController.setStatusBarColor(color = Color.Transparent, darkIcons = darkIcons)
+    })
+}
